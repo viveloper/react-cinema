973d9bc59a40cdfa092771cdc66447cb6e4e82e0
viveloper <usik1234@gmail.com>
2020-05-06 00:32:19 +0900

    added FilteringTabs Component

--- src/components/Ticketing/FilteringTabs.jsx
+++ src/components/Ticketing/FilteringTabs.jsx
@@ -1,7 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import classes from './FilteringTabs.module.css';
 const FilteringTabs = () => {
-  return <div>FilteringTabs</div>;
+  const [activeTab, setActiveTab] = useState('special');
+  const handleClick = (tabName) => {
+    setActiveTab(tabName);
+  };
+  return (
+    <div className={classes['filtering-tabs']}>
+      <button
+        className={
+          activeTab === 'all'
+            ? `${classes['btn-tab']} ${classes['active']}`
+            : classes['btn-tab']
+        }
+        onClick={() => handleClick('all')}
+      >
+        전체
+      </button>
+      <button
+        className={
+          activeTab === 'special'
+            ? `${classes['btn-tab']} ${classes['active']}`
+            : classes['btn-tab']
+        }
+        onClick={() => handleClick('special')}
+      >
+        스페셜관
+      </button>
+    </div>
+  );
 };
 
 export default FilteringTabs;
