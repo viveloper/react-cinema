35503a3f3c3868fdd589bc498356cdd1c1924fc7
viveloper <usik1234@gmail.com>
2020-08-04 07:35:00 +0900

    updated package.json

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-axios.defaults.baseURL = process.env.REACT_APP_BASE_URL;
+// axios.defaults.baseURL = process.env.REACT_APP_BASE_URL;
 
 export const getUser = async (token) => {
   const res = await axios.get(`/api/user`, {
